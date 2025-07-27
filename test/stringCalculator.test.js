267ad26d584bc1bc8eaacef30f749d48a4ee56e0
HareshParmar <hareshparmar32@gmail.com>
2025-07-27 18:07:24 +0530

    add test for handling newlines between numbers

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -15,3 +15,7 @@ test('two comma separated numbers return their sum', () => {
 test('handles unknown amount of comma separated numbers', () => {
   expect(add("1,2,3,4")).toBe(10);
 });
+
+test('handles newlines between  numbers', () => {
+  expect(add("1\n2,3")).toBe(6);
+})
