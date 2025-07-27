9f8027071a3f422fa4853c08fc509440262c6787
HareshParmar <hareshparmar32@gmail.com>
2025-07-27 18:15:04 +0530

    add test for custom delimiter support

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -18,4 +18,8 @@ test('handles unknown amount of comma separated numbers', () => {
 
 test('handles newlines between  numbers', () => {
   expect(add("1\n2,3")).toBe(6);
-})
+});
+
+test('supports custom delimiter', () => {
+  expect(add("//;\n1;2")).toBe(3);
+});
