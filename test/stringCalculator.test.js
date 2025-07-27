c9df8361b3e964d0310756a7014f203219020e57
HareshParmar <hareshparmar32@gmail.com>
2025-07-27 16:07:07 +0530

    add test for single number input returning itself

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -3,3 +3,7 @@ const { add } = require('../src/stringCalculator');
 test('empty string returns 0', () => {
   expect(add("")).toBe(0);
 });
+
+test('single number returning itself', () => {
+  expect(add("1")).toBe(1);
+});
