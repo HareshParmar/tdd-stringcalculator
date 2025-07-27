8c6ac3b49a9869cf8bdc46dc74257791ab2419ef
HareshParmar <hareshparmar32@gmail.com>
2025-07-27 17:58:08 +0530

    add test for two comma separated numbers returning their sum

--- test/stringCalculator.test.js
+++ test/stringCalculator.test.js
@@ -7,3 +7,7 @@ test('empty string returns 0', () => {
 test('single number returning itself', () => {
   expect(add("1")).toBe(1);
 });
+
+test('two comma separated numbers return their sum', () => {
+  expect(add("1,2")).toBe(3);
+});
